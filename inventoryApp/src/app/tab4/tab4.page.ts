446c8e669ecb8f929b583b2a329100647cd84a0d
Kstewart57 <k.stewart.35@student.scu.edu.au>
2025-12-07 09:48:27 +1000

    Update card headers to use IonCardTitle
    Replaced IonCardSubtitle with IonCardTitle in tab4.page.html for better semantic structure. Updated tab4.page.ts to import IonCardTitle and related components.

--- inventoryApp/src/app/tab4/tab4.page.ts
+++ inventoryApp/src/app/tab4/tab4.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: 'tab4.page.html',
   styleUrls: ['tab4.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent],
 })
 export class Tab4Page {
   constructor() {}
