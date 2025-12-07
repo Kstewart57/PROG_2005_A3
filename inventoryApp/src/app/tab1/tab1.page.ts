55f20bc4068d00003ac9c1788084ebe9fa5e8ffd
Steve <s.travers.10@student.scu.edu.au>
2025-12-07 14:02:29 +1100

    Update tab1.page.ts
    made help widget the same across all pages

--- inventoryApp/src/app/tab1/tab1.page.ts
+++ inventoryApp/src/app/tab1/tab1.page.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { InventoryService } from '../services/inventory.service';
 import { InventoryItem } from '../models/inventory-item.model';
+import { AlertController } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-tab1',
@@ -18,7 +20,8 @@ export class Tab1Page implements OnInit {
   loading = true; // Controls loading message display
   showHelp = false; // Controls help visibility
 
-  constructor(private inventoryService: InventoryService) {}
+  constructor(private inventoryService: InventoryService,
+    private alertController: AlertController ) {}
 
   // Fetch all inventory items on page load
   ngOnInit() {
@@ -57,4 +60,21 @@ export class Tab1Page implements OnInit {
   toggleHelp() {
     this.showHelp = !this.showHelp;
   }
+
+
+// show help information
+async showHelpAlert(): Promise<void> {
+  const alert = await this.alertController.create({
+    header: 'Update / Delete help',
+    message:
+      '• Enter an item name and click Search to find specific items.\n' +
+      '• Click Refresh to reset the search and display all items.\n' +
+      '• View all inventory data including ID, name, category, quantity, price, supplier, stock status, featured status, and notes.\n',
+    buttons: ['OK'],
+    cssClass: 'help-alert'
+  });
+
+  await alert.present();
+}
+
 }
