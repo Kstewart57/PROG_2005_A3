49c2164a35a94352c43b96249b8da34334d20a1b
Steve <s.travers.10@student.scu.edu.au>
2025-12-07 16:10:05 +1100

    Update tabs.page.ts
    added icons for tabs

--- inventoryApp/src/app/tabs/tabs.page.ts
+++ inventoryApp/src/app/tabs/tabs.page.ts
@@ -1,18 +1,29 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+import {
+  readerOutline,
+  duplicateOutline,
+  buildOutline,
+  peopleOutline
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
+  standalone: true,
   imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square });
+    addIcons({
+      'reader-outline': readerOutline,
+      'duplicate-outline': duplicateOutline,
+      'build-outline': buildOutline,
+      'people-outline': peopleOutline
+    });
   }
 }
